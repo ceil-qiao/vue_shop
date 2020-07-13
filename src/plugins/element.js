924e10f65f2332884618187f541940daa72b3a9a
qiao <15930299650@163.com>
2020-07-13 15:28:45 +0800

    完成用户列表功能的开发

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -3,13 +3,52 @@ import {
     Button,
     Form ,
     FormItem,
-    input,
-    Message
-}from 'element-ui'
+    Input,
+    Message,
+    Container,
+    Header,
+    Aside,
+    Main,
+    Menu,
+    Submenu,
+    MenuItem,
+    Breadcrumb,
+    BreadcrumbItem,
+    Card,
+    Row,
+    Col,
+    Table,
+    TableColumn,
+    Switch,
+    Tooltip,
+    Pagination,
+    Dialog,
+    MessageBox,
+}
+from 'element-ui'
 //注册为全局可用的组件
 Vue.use(Button)
 Vue.use(Form)
 Vue.use(FormItem)
-Vue.use(input)
+Vue.use(Input)
+Vue.use(Container)
+Vue.use(Header)
+Vue.use(Aside)
+Vue.use(Main)
+Vue.use(Menu)
+Vue.use(Submenu)
+Vue.use(MenuItem)
+Vue.use(Breadcrumb)
+Vue.use(BreadcrumbItem)
+Vue.use(Card)
+Vue.use(Row)
+Vue.use(Col)
+Vue.use(Table)
+Vue.use(TableColumn)
+Vue.use(Switch)
+Vue.use(Tooltip)
+Vue.use(Pagination)
+Vue.use(Dialog)
 //弹窗需要全局挂载,弹窗组件挂载到原型对象，每一个原型组件都可以通过this法昂问$message ,弹窗提示
 Vue.prototype.$message = Message
+Vue.prototype.$confirm=MessageBox.confirm
--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,24 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Login from '../components/Login.vue'
-import Home from '../components/Home'
-
+import Home from '../components/Home.vue'
+import Welcome from '../components/Welcome.vue'
+import Users from '../components/user/Users.vue'
 
 Vue.use(VueRouter)
   const routes = [
     {path:'/',redirect:'/login'},//设置重定向
     {path:'/login', component: Login},
-    {path:'/home', component: Home},
+    //在home 中嵌套子路由，welcome在main中显示welcome
+    { path:'/home',
+      component: Home ,
+      redirect:'/welcome',
+      children:[
+        {path :'/welcome', component:Welcome},
+        {path :'/users',component:Users}
+      ],
+    },
+
 
 ]
 const router = new VueRouter({
